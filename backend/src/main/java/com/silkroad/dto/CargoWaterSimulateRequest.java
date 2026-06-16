0b6bb5b9ce4107326e0cc94504951ed54b1adff0
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-16 09:33:37 +0800

    Update project

---
+++ backend/src/main/java/com/silkroad/dto/CargoWaterSimulateRequest.java
@@ -0,0 +1,27 @@
+package com.silkroad.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CargoWaterSimulateRequest {
+
+    private String cargoType;
+
+    private Integer camelCount;
+
+    private Integer crewCount;
+
+    private Double cargoWeightKg;
+
+    private Integer days;
+
+    private String terrainType;
+
+    private Double temperatureC;
+}
